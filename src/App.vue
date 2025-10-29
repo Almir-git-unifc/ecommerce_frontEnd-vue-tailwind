096e138026a2cd6364f83962e4a45ab4f78e4ede
Almir-Git <81040770+Almir-git-unifc@users.noreply.github.com>
2025-10-28 22:09:29 -0300

    feat(Vue): v00.02.000 - make section Best Deals

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template  lang="">
   <div>
     <Header />
+    <BestDeals />
   </div>
 </template>
 
 <script setup>
     import Header from "./components/Header.vue";
+    import BestDeals from "./components/BestDeals.vue";
 </script>
 
 
